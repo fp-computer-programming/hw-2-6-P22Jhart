89aff239b53a4fd59825d91c2216ff00064530cd
Jordan Hart <p22jhart@fairfieldprep.org>
2021-09-26 22:36:41 -0400

    s

---
+++ hw 2-6-1.py
@@ -0,0 +1,5 @@
+fahrenheit = 20
+
+celsius = (fahrenheit - 32) / 1.8
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
---
+++ hw 2-6-3.py
@@ -0,0 +1,7 @@
+freethrow = input("Free throws")
+
+twopoint = input("2 pointer count")
+
+threepointers = input("3 pointer count")
+
+points = freethrow + 2 * (twopoint) = 3 * (threepointers)
